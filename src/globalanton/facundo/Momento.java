367c2edbbdfdce2b90b9e9438f640d2e1ef5809e
CresciniF <104316920+CresciniF@users.noreply.github.com>
2022-06-24 19:42:24 -0300

    aqqweqe
    qweqwer

---
+++ src/globalanton/facundo/Momento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package globalanton.facundo;
+
+/**
+ *
+ * @author Facundo
+ */
+public class Momento {
+    
+}
